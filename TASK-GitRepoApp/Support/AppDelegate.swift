c62cefbecbbed3a3523bf9680fd7c335cf5c56ba
Tomislav Gelesic <gelesic.tomislav@gmail.com>
2021-03-01 13:34:17 +0100

    init

---
+++ TASK-GitRepoApp/Support/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TASK-GitRepoApp
+//
+//  Created by Tomislav Gelesic on 01.03.2021..
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        return true
+    }
+
+
+}
+
