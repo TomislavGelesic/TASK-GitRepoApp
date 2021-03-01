1c8b49dc763eda2e6de2d1f49646616962879136
Tomislav Gelesic <gelesic.tomislav@gmail.com>
2021-03-01 13:22:07 +0100

    Initial Commit

---
+++ TASK-GitRepoAppTests/TASK_GitRepoAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  TASK_GitRepoAppTests.swift
+//  TASK-GitRepoAppTests
+//
+//  Created by Tomislav Gelesic on 01.03.2021..
+//
+
+import XCTest
+@testable import TASK_GitRepoApp
+
+class TASK_GitRepoAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
