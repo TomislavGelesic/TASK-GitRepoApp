df1af4f4a21e74ed0c99d2361394018315ecdd94
Tomislav Gelesic <gelesic.tomislav@gmail.com>
2021-03-05 11:42:01 +0100

    safety

--- TASK-GitRepoApp/Common/Constants.swift
+++ TASK-GitRepoApp/Common/Constants.swift
@@ -11,4 +11,13 @@ struct Constants {
     static let GITHUB_BASE = "https://api.github.com"
     static let SEARCH_REPOSITORIES = "/search/repositories?q="
     static let SEARCH_USERS = "/search/users?q="
+    
+
+    //    mock users response
+    //    https://api.github.com/search/repositories?q=Technology
+
+    //    mock repositories response
+    //    https://api.github.com/search/repositories?q=
 }
+
+
--- TASK-GitRepoApp/DetailsScreen/View/DetailsScreenViewController.swift
+++ TASK-GitRepoApp/DetailsScreen/View/DetailsScreenViewController.swift
@@ -15,9 +15,7 @@ class DetailsScreenViewController: UIViewController {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
-    
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
-    
     deinit { print("DetailsScreenViewController deinit called.") }
     
     override func viewDidLoad() {
@@ -27,8 +25,6 @@ class DetailsScreenViewController: UIViewController {
         setConstraintsWebView()
         showWebPage()
     }
-    
-
 }
 
 extension DetailsScreenViewController {
@@ -43,19 +39,13 @@ extension DetailsScreenViewController {
         navigationItem.setLeftBarButton(backButton, animated: true)
         navigationController?.navigationBar.tintColor = .init(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
         navigationItem.title = viewModel.screenData.title
-        
     }
     func setupViews() {
         webView.uiDelegate = self
         view.addSubview(webView)
     }
-    
-    @objc func backTapped() {
-        viewModel.backButtonTapped() }
-    
-    func showWebPage() {
-        if let validPath = viewModel.screenData.webPagePath { webView.load(validPath) }
-    }
+    @objc func backTapped() { viewModel.backButtonTapped() }
+    func showWebPage() { if let validPath = viewModel.screenData.webPagePath { webView.load(validPath) } }
 }
 
 extension DetailsScreenViewController: WKUIDelegate { }
@@ -63,7 +53,6 @@ extension DetailsScreenViewController: WKUIDelegate { }
 
 extension DetailsScreenViewController {
     //MARK: CONSTRAINTS BELOW
-    
     func setConstraintsWebView() {
         webView.snp.makeConstraints { (make) in make.edges.equalTo(view.safeAreaLayoutGuide) }
     }
